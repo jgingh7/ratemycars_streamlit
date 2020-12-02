f2e4abcccdc53d469f265534e4f9ebaeedb9f093
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-12-02 17:15:24 -0500

    finish 4

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import psycopg2
 import streamlit as st
 from configparser import ConfigParser
 
-'# Demo: Streamlit + Postgres'
+'# Project: Cars, Tires, Rating'
 
 
 @st.cache
@@ -45,47 +45,114 @@ def query_db(sql: str):
     return df
 
 
-'## Read tables'
-
-sql_all_table_names = "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';"
-all_table_names = query_db(sql_all_table_names)['relname'].tolist()
-table_name = st.selectbox('Choose a table', all_table_names)
-if table_name:
-    f'Display the table'
-
-    sql_table = f'select * from {table_name};'
-    df = query_db(sql_table)
-    st.dataframe(df)
-
 # Query for cars with certain tires
-'## Query for cars with tires'
-
+'## Query for cars with selected tires'
 sql_tire_info = 'select tire_id, model from tires_made_from;'
 tire_info = query_db(sql_tire_info)
 tire_ids, tire_models = tire_info['tire_id'].tolist(), tire_info['model'].tolist()
 
-tire_ids_names = [a + ' : ' + str(b) for a, b in zip(tire_models, tire_ids)]
+tire_ids_models = [a + ' : ' + str(b) for a, b in zip(tire_models, tire_ids)]
 
-selection = st.selectbox(
-    'Choose a tire (Tire Model: Tire ID) to look for cars that use the selected tire.', tire_ids_names)
+selection = st.multiselect(
+    'Choose tires (Tire Model: Tire ID) to look for cars that use the selected tire.', tire_ids_models)
 if selection:
-    tire_id = selection.split(':')[1].strip()
+    tire_id = [a.split(':')[1].strip() for a in selection]
+    tire_id_str = ','.join([str(elem) for elem in tire_id])
 
-    sql_cars = f"""select C.model, C.year, T.model, T.type, T.size
+    sql_cars = f"""select C.model as "car model", C.year as "car model year", T.model as "tire model", T.type as "tire type", T.size as "tire size"
                     from cars_made_from_come_with C, tires_made_from T
-                    where id_tire={tire_id} and tire_id={tire_id};"""
+                    where C.id_tire in ({tire_id_str}) and T.tire_id in ({tire_id_str}) order by T.tire_id;"""
     car_info = query_db(sql_cars)
     if not car_info.empty:
         st.dataframe(car_info)
     else:
-        st.write('No cars.')
+        st.write('No cars with selected tires.')
 
 
 # Query for tires with the highest car ratings
-# select T.tire_id, T.model, round(avg(R.num_stars),2) AS average_star_rating from ratings_rate R, cars_made_from_come_with C, tires_made_from T where C.id_tire=T.tire_id and R.id_car=C.car_id group
-#  by T.tire_id order by average_star_rating desc;
-'## Query for tires with the highest car ratings'
+'## Top 10 tires with the highest car ratings'
+sql_top_rate_tire = f"""select T.model, round(avg(R.num_stars),2) as "average rating"
+                        from ratings_rate R, cars_made_from_come_with C, tires_made_from T
+                        where C.id_tire=T.tire_id and R.id_car=C.car_id group by T.tire_id order by "average rating" desc limit 10;"""
+top_rate_tire_info = query_db(sql_top_rate_tire)
+if not top_rate_tire_info.empty:
+    st.dataframe(top_rate_tire_info)
+else:
+    st.write('No cars or no tires.')
 
+# See cars sold by a specific dealer
+'## Query for cars by sold by selected dealers'
+sql_dealer_info = 'select dealer_id, name from dealers;'
+dealer_info = query_db(sql_dealer_info)
+dealer_ids, dealer_names = dealer_info['dealer_id'].tolist(), dealer_info['name'].tolist()
+
+dealer_ids_names = [a + ' : ' + str(b)
+                    for a, b in zip(dealer_names, dealer_ids)]
+
+selection = st.selectbox('Choose a dealer', dealer_ids_names)
+if selection:
+    dealer_id = selection.split(':')[1].strip()
+    dealer_name = selection.split(':')[0].strip()
+
+    sql_cars = f"""select C.model as car_model, C.year as car_model_year, D.name as dealer_name, D.state as dealer_state, D.city as dealer_city
+                    from cars_made_from_come_with C, sell S, dealers D
+                    where S.id_car = C.car_id and S.id_dealer = {dealer_id} and D.dealer_id = {dealer_id};"""
+    car_models, car_model_years = query_db(sql_cars)['car_model'].tolist(), query_db(sql_cars)['car_model_year'].tolist()
+
+    car_models_years = [a + ', ' + str(b)
+                        for a, b in zip(car_models, car_model_years)]
+    car_models_years_str = '\n\n'.join(
+        [str(elem) for elem in car_models_years])
+
+    st.write(
+        f"The below cars are sold by dealer '{dealer_name}'\n\n {car_models_years_str}")
+#TOTAL HOW MANY CARS?
+
+# Show the ratings received by a car
+'## Query for the ratings of a selected car'
+sql_car_info = 'select car_id, model, year from cars_made_from_come_with;'
+car_info = query_db(sql_car_info)
+car_ids, car_models, car_model_years = car_info['car_id'].tolist(), car_info['model'].tolist(), car_info['year'].tolist()
+
+car_models_years = [a + ', ' + str(b)
+                        for a, b in zip(car_models, car_model_years)]
+car_ids_models = [a + ' : ' + str(b)
+                    for a, b in zip(car_models_years, car_ids)]
+
+selection = st.selectbox('Choose a car', car_ids_models)
+if selection:
+    car_id = selection.split(':')[1].strip()
+    sql_ratings = f"""select R.num_stars, R.comment, R.time_created
+                        from cars_made_from_come_with C, ratings_rate R
+                        where C.car_id = {car_id} and R.id_car = {car_id};"""
+    rating_info = query_db(sql_ratings)
+    if not rating_info.empty:
+        st.dataframe(rating_info)
+    else:
+        st.write('No ratings exists.')
+#WHAT IS THE AVERAGE RATING?
+
+
+# List the top 3 rated cars for each manufacturer, along with the average rating of each car
+# List top 3 tire manufacturers with highest rated cars on average
+# List top 10 users who rated for japanese cars
+
+
+
+
+
+
+# '## Read tables'
+
+# sql_all_table_names = "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';"
+# all_table_names = query_db(sql_all_table_names)['relname'].tolist()
+# table_name = st.selectbox('Choose a table', all_table_names)
+# if table_name:
+#     f'Display the table'
+
+#     sql_table = f'select * from {table_name};'
+#     df = query_db(sql_table)
+#     st.dataframe(df)
 
 # '## Query customers'
 
